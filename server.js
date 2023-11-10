b80d00cd4020dfa1b7dc59f68c19a59a00ed490d
nijatori <garruco@live.com.pt>
2023-11-10 14:40:17 +0000

    Teste movimento

--- server.js
+++ server.js
@@ -1,59 +1,63 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const path = require("path"); // Adicionado para lidar com caminhos de diretório
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve arquivos estáticos do diretório 'public'
 app.use(express.static(path.join(__dirname, "public")));
 
-// Serve a página principal (ecrã partilhado)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-// Serve a interface do telemóvel
 app.get("/mobile", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/mobile.html"));
 });
 
-// Lista de jogadores conectados
-let players = [];
+// Cores disponíveis para os jogadores
+const availableColors = ["red", "blue", "green", "yellow"];
+let players = {};
 
 io.on("connection", (socket) => {
   console.log("Um jogador conectou-se:", socket.id);
 
-  // Adicionar jogador à lista
-  players.push(socket.id);
-  if (players.length > 3) {
+  if (Object.keys(players).length >= 3) {
     socket.emit("error", "Máximo de 3 jogadores atingido");
     socket.disconnect();
     return;
   }
 
-  // Enviar atualizações para o ecrã partilhado
+  // Atribuir uma cor ao jogador
+  const playerColor = availableColors.pop();
+  if (!playerColor) {
+    socket.emit("error", "Não há mais cores disponíveis");
+    socket.disconnect();
+    return;
+  }
+  socket.emit("yourColor", playerColor);
+
+  players[socket.id] = { x: 0, y: 0, color: playerColor };
+
   io.emit("updateScreen", { players });
 
-  // Ouvir movimentos do jogador
   socket.on("move", (data) => {
-    console.log(
-      `Jogador ${socket.id} quer ${data.action} na direção ${data.direction}`
-    );
-
-    // Aqui, pode adicionar lógica para processar o movimento, como atualizar a posição do personagem
-
-    // Enviar atualizações para todos os jogadores e ecrã partilhado
-    io.emit("updateGame", {
-      message: `Jogador ${socket.id} moveu-se para ${data.direction}`,
-    });
+    if (players[socket.id]) {
+      players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
+      io.emit("updateGame", { players });
+    }
   });
 
   socket.on("disconnect", () => {
     console.log("Jogador desconectou-se:", socket.id);
-    players = players.filter((player) => player !== socket.id);
+    // Devolver a cor ao conjunto de cores disponíveis
+    if (players[socket.id]) {
+      availableColors.push(players[socket.id].color);
+    }
+    delete players[socket.id];
     io.emit("updateScreen", { players });
   });
 });
