ce3b57d1a08bd2cfd6dde71b5e56c83f7b46ca15
antoniooooooooooooooooo <apedrosimoes39@gmail.com>
2023-12-11 20:26:30 +0000

    update em contexto

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
+app.get("/jogo", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/jogo.html"));
 });
 
@@ -21,10 +21,15 @@ app.get("/context", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/context.html"));
 });
 
+app.get("/context_mobile", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/context_mobile.html"));
+});
 // Cores disponíveis para os jogadores
 const availableColors = ["red", "blue", "green", "yellow"];
 let players = {};
 let connectedPlayersCount = 0;
+let playersClickedContinue1 = 0;
+let playersClickedContinue2 = 0;
 
 io.on("connection", (socket) => {
   console.log("Um jogador conectou-se:", socket.id);
@@ -47,6 +52,8 @@ io.on("connection", (socket) => {
   // Envie entrada do player para o frontend
   io.emit("addPlayer", socket.id);
 
+  
+
   // characters
   socket.on("submit", (data) => {
     if (players[socket.id]) {
@@ -56,8 +63,38 @@ io.on("connection", (socket) => {
       console.log(players[socket.id].char);
       io.emit("submit", socket.id);
       io.emit("submit", { players });
+  
+      // Increment the count when a player clicks "Continue1"
+      playersClickedContinue1++;
+  
+      // If all three players clicked "Continue1", emit an event to switch context
+      if (playersClickedContinue1 === 3) {
+        io.emit("switchContext", "/context2");
+      }
+
+      playersClickedContinue2++;
+
+      // If all three players clicked "Continue2", emit an event to switch context to jogo.html
+      if (playersClickedContinue2 === 3) {
+        io.emit("switchContext", "/jogo");
+      }
+      
     }
   });
+  
+
+  socket.on("continue1Clicked", () => {
+    socket.broadcast.emit("continue1Clicked");
+  });
+
+  socket.on("continue2Clicked", () => {
+    playersClickedContinue2++;
+
+    if (playersClickedContinue2 === 3) {
+    io.emit("switchContext", "/jogo");
+  }
+  });
+  
 
   // mover
   socket.on("move", (data) => {
